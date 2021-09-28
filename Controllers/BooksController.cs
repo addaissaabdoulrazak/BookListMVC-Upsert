6f972919dd67218b9ea69ad80fc1e87591b67f4b
Adda Issa Abdoul Razak <addaissaabdoulrazak@gmail.com>
2021-09-28 02:56:22 +0200

    Upsert with MVC

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,88 @@
+﻿using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(int?id)
+        {
+            Book = new Book();
+            if(id==null)
+            {
+                //create
+                return View(Book);
+            }
+            //a ce niveau vous pouvez utiliser la methode Find() en lui passant juste le parametre id par contre si vous utiliser la Fonction FirstOrDefault()
+            //comme Methode vous allez devoir utiliser u=>u.Id==id
+            Book = _db.Book.FirstOrDefault(u => u.Id == id);
+                return View(Book);
+        } 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if(ModelState.IsValid)
+            {
+                if(Book.Id==0)
+                {
+                    //Create
+                    _db.Book.Add(Book);
+                }
+                else
+                {
+                    //Update
+                    _db.Book.Update(Book);
+                }
+                // pui en dernière Position nous allons =>poussez<= les données dans la Base de données
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //dans le cas ou le Model n'est pas Valide 9a me Gardera sur la même Page avec Les même données Invalid
+            return View(Book);
+        }
+
+        #region Api Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Book.ToList() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            //recuperer un livre de la base de données en function de l'identifiant
+            var BookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+
+            //Chaque fois que vous effectuer un acces a la database pour une Modification il est capital d'effectuer un Test
+
+            if (BookFromDb == null)
+            {
+                return Json(new { success = false, Message = "Impossible de supprimer un livre qui n'existe pas dans la base de données" });
+            }
+            _db.Book.Remove(BookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "suppression effectuer avec succès" });
+        }
+        #endregion
+    }
+}
---
+++ wwwroot/js/BookList.js
@@ -0,0 +1,115 @@
+﻿
+var dataTable;
+
+$(Document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+
+    dataTable = $('#DT_load').dataTable({
+
+        "ajax": {
+            "url": "/Books/Getall/",
+            "type": "GET",
+            "datatype": "json"
+        },
+
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class= "text-center">
+                             <a href="/Books/Upsert?id=${data}" class= "btn btn-success text-white" style="cusor:pointer; width=70%;">Edit</a>
+                         &nbsp;
+                             <a  class= "btn btn-danger text-white" style="cusor:pointer; width=70%;" onclick=Delete('/Books/Delete?id='+${data})>Delete</a>
+
+                            </div>`
+                }, "width": "40%",
+            }
+
+
+
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+
+
+
+
+
+
+    });
+}
+function Delete(url) {
+    //copie Coller du code Swet-Alert
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function(data){
+                    if (data.success) {
+
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                        datatable.ajax.reload();
+                    }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+
+
+
+
+
+            })
+
+
+
+}
+
+function Delete(url) {
+
+    //utilisation de sweet-Alert
+   
+            $.ajax({
+
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                        datatable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+
+                }
+            });
+        }
+    });
+
+}
